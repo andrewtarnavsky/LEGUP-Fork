d1edf5ed1220f2c6899ad8981358b50f6dcdba68
sdhoat <121777913+sdhoat@users.noreply.github.com>
2025-04-21 13:56:43 -0400

    Update YinYangCell.java

--- src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangCell.java
+++ src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangCell.java
@@ -1,54 +1,52 @@
 package edu.rpi.legup.puzzle.yinyang;
 
 import edu.rpi.legup.model.gameboard.GridCell;
+import edu.rpi.legup.model.elements.PlaceableElement;
+import java.awt.event.MouseEvent;
 
 public class YinYangCell extends GridCell<YinYangType> {
 
     public YinYangCell(YinYangType type, int x, int y) {
-        super(type, x, y); // Use YinYangType directly
+        super(type, x, y);
     }
 
-    /**
-     * Gets the type of the cell.
-     *
-     * @return the YinYangType of the cell
-     */
+    /** Gets the type of the cell. */
     public YinYangType getType() {
         return getData();
     }
 
-    /**
-     * Sets the type of the cell.
-     *
-     * @param type the YinYangType to set
-     */
+    /** Sets the type of the cell. */
     public void setType(YinYangType type) {
         setData(type);
     }
 
     /**
-     * Gets the X coordinate of the cell.
-     *
-     * @return the X coordinate
+     * Sets the cell's type based on a selected puzzle editor element.
+     * Allows placement of black/white circles or clearing to unknown.
      */
+    public void setType(PlaceableElement element, MouseEvent e) {
+        String name = element.getElementName();
+        if ("Black Circle".equals(name)) {
+            this.setType(YinYangType.BLACK);
+        } else if ("White Circle".equals(name)) {
+            this.setType(YinYangType.WHITE);
+        } else {
+            // Any other selection clears the cell to unknown
+            this.setType(YinYangType.UNKNOWN);
+        }
+    }
+
+    /** Gets the X coordinate of the cell. */
     public int getX() {
         return location.x;
     }
 
-    /**
-     * Gets the Y coordinate of the cell.
-     *
-     * @return the Y coordinate
-     */
+    /** Gets the Y coordinate of the cell. */
     public int getY() {
         return location.y;
     }
 
-    /**
-     * Performs a deep copy of the YinYangCell.
-     *
-     * @return a new copy of the YinYangCell
-     */
+    /** Performs a deep copy of the YinYangCell. */
     @Override
     public YinYangCell copy() {
         return new YinYangCell(getType(), location.x, location.y);
