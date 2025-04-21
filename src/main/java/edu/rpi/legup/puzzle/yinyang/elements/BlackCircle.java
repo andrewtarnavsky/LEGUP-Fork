6c6c3c577ab36b7d968e726533d43405bb74db38
sdhoat <121777913+sdhoat@users.noreply.github.com>
2025-04-21 13:51:47 -0400

    Add Black & White Circle

---
+++ src/main/java/edu/rpi/legup/puzzle/yinyang/elements/BlackCircle.java
@@ -0,0 +1,10 @@
+package edu.rpi.legup.puzzle.yinyang.elements;
+
+import edu.rpi.legup.model.elements.PlaceableElement;
+
+public class BlackCircle extends PlaceableElement {
+    public BlackCircle() {
+        super("YINY-ELEM-0002", "Black Circle", "A black circle tile",
+                "edu/rpi/legup/images/yinyang/tiles/BlackCircle.png");
+    }
+}
---
+++ src/main/java/edu/rpi/legup/puzzle/yinyang/elements/WhiteCircle.java
@@ -0,0 +1,10 @@
+package edu.rpi.legup.puzzle.yinyang.elements;
+
+import edu.rpi.legup.model.elements.PlaceableElement;
+
+public class WhiteCircle extends PlaceableElement {
+    public WhiteCircle() {
+        super("YINY-ELEM-0001", "White Circle", "A white circle tile",
+                "edu/rpi/legup/images/yinyang/tiles/WhiteCircle.png");
+    }
+}
